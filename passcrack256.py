7a263339d0cbd4ce1aa1e380eea4ddeaa5e1129f
fhenry9618 <f.fredhenry@gmail.com>
2022-09-07 21:15:31 -0500

    added functions to main file, also checking compliance with .gitignore_global rules

--- passcrack256.py
+++ passcrack256.py
@@ -1,3 +1,14 @@
 #!/bin/python3
 
-print("text test")
+from pwn import *
+import sys
+
+# input checker - very rudimentary validation
+if len(sys.argv) != 2:
+    print("Invalid argument.")
+    print(">> {} <sha256sum>".format(sys.argv[0]))
+    exit()
+
+target_hash = sys.argv[1]
+
+password_bank = "rockyou.txt"
